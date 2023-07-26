63c058fc6f4964d90ae53cc9cfb18209eb19f15e
Nimo08 <nimomohamed800@gmail.com>
2023-07-26 22:40:19 +0300

    Added div.c to get_op.c and monty.h

---
+++ div.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * div - divides second top element of stack by top element of stack
+ * @stack: pointer to pointer
+ * @line_number: line number
+ * Return: nothing
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr2;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr2 = (*stack)->next;
+	if (*stack == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ptr2->n = ptr2->n / (*stack)->n;
+	pop(&(*stack), line_number);
+}
--- monty.h
+++ monty.h
@@ -59,6 +59,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
 void parse_line(char *line);
 void exit_status(void);
 
